49ce38ba5f8b037e5d693f4dd58bdbcb2e8ffe99
Viglot <vigviglot36.hiltunen@gmail.com>
2021-11-18 09:13:52 +0200

    4

--- app.js
+++ app.js
@@ -1,19 +1,54 @@
-const todos = ["Gå till skolan", "Programmera", "Köpa mat" ];
+const input = document.getElementById('todo');
+const button = document.getElementById('add')
+const list = document.getElementById('list');
 
-for (let i = 0; i < 1; i++) {
-let li = document.createElement('li')
-li.textContent = 'Gå till skolan';
-list.appendChild(li);
+const todos = [
+  {text: 'hej på dej', done: false},
+  {text: 'Programmera', done: true},
+  {text: 'gå hem', done: false}
+];
+
+button.addEventListener('click', onButtonClicked);
+
+updateView();
+
+function onButtonClicked() {
+
+  // Arrayfunktionen push lägger till en item till arrayns sista plats.
+  todos.push({
+    text: input.value,
+done: false
+  });
+
+  updateView();
+
 }
 
-for (let i = 1; i < 2; i++) {
-    let li = document.createElement('li')
-    li.textContent = 'Programmera';
-    list.appendChild(li);
-    }
+function updateView(){
 
-    for (let i = 2; i < 3; i++) {
-        let li = document.createElement('li')
-        li.textContent = 'Köpa Mat';
-        list.appendChild(li);
-        }
+  list.innerHTML = '';
+  for(let i = 0; i < todos.length; i++)
+{
+  // 1. Skapa ett li element: <li></li>
+  const li = document.createElement('li');
+  li.dataset.index = i;
+  // 2. Ändra på texten inuti li-elementet: <li>Programmera</li>
+  li.textContent = todos[i].text;
+  // 3. Lägg till li-elementet till vårt ul-element
+  if(todos[i].done){
+    li.style.textDecoration = 'line-through'
+  }
+  list.appendChild(li);
+
+  li.addEventListener('click', onItemClicked);
+}
+}
+
+
+function onItemClicked(e) {
+
+  todos[e.target.dataset.index].done = !todos[e.target.dataset.index].done;
+  
+
+  updateView();
+}
